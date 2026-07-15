083822f0bbb541e2b71a58f5c708c189cb68a96a
asit singha <asit20singha02@gmail.com>
2026-07-16 01:15:03 +0530

    fix minor multer bug

--- vidStream_backend/src/middlewares/multer.middleware.js
+++ vidStream_backend/src/middlewares/multer.middleware.js
@@ -1,22 +1,21 @@
-import multer from "multer";
+import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
+// Create temp directory if it doesn't exist
 const tempDir = 'public/temp';
 if (!fs.existsSync(tempDir)) {
   fs.mkdirSync(tempDir, { recursive: true });
 }
 
+// Configure multer storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "tempDir")
-    },
-    filename: function (req, file, cb) {
-      
-     cb(null, Date.now() + '-' + file.originalname);
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+export const upload = multer({ storage });
