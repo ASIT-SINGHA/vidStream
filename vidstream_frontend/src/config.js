a3e1e2aa8d67d12e300a696c94c4bd9d3405bd4a
asit singha <asit20singha02@gmail.com>
2026-08-15 23:19:31 +0530

    Add configuration file for API base URLs

---
+++ vidstream_frontend/src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    viteLocalUrl:String(import.meta.env.VITE_API_BASE_URL),
+    viteVidStreamRenderUrl:String(import.meta.env.VITE_API_VIDSTREAM_URL_RENDER)
+}
+export default config;
